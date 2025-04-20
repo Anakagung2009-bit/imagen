39329fb3f6c1f558fe947ad2da38907d87287fcc
Anakagung2009-bit <agungdevofficial@gmail.com>
2025-04-20 18:03:12 +0800

    Update useCurrentUser.ts

--- lib/useCurrentUser.ts
+++ lib/useCurrentUser.ts
@@ -1,7 +1,7 @@
 // lib/hooks/useCurrentUser.ts
 import { useState, useEffect } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { app } from "@/lib/firebase"; // atau path yang sesuai
+import app from "@/lib/firebase"; // atau path yang sesuai
 import type { User } from "firebase/auth";
 
 export function useCurrentUser() {
